beb672844a76fcaf7ee0836eaf596e60790ee653
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-05-07 01:36:43 +0200

    fix dist file in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import path, { resolve } from 'path'
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 const base = mode === 'production' ? '/' + path.basename(process.cwd()) + '/' : '/'
 export default defineConfig({
     root: 'src',
+    mode,
+    base,
     build: {
-        outDir: '../dist'
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                baseball: resolve(__dirname, 'src/baseball.html'),
+                express: resolve(__dirname, 'src/app.js')
+            }
+        }
     },
     server: {
         // https: true,
