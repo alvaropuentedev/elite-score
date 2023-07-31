0bfb760f10e62cd21308417cb5dfe0b3a148c6a1
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-07-31 02:19:32 +0200

    Fix day = 32

--- src/js/baseball.js
+++ src/js/baseball.js
@@ -26,10 +26,14 @@ const toDateTime = (secs) => {
     let hours = t.getHours() + 2
     const minutes = String(t.getMinutes()).padStart(2, '0')
     let day = t.getDate()
-    const month = t.getMonth() + 1
+    let month = t.getMonth() + 1
     hours = hours % 24
     if (hours === 0 || hours === 1) {
         day = day + 1
+        if (day === 32) {
+            day = 1
+            month = month + 1
+        }
     }
     return day + '/' + month + '  ' + hours + ':' + minutes
 }
